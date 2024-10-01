225f8b80b15bc496fea92b5b4f522bbca2e607e2
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-01 22:04:36 +0530

    Day_02.py
    Added input taking

--- Codes/Day_02.py
+++ Codes/Day_02.py
@@ -10,3 +10,13 @@ c = 1.9
 d = 8
 
 print(c + d)
+
+# Taking User Input
+a = input("Enter your name: ")
+print("My name is", a)
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+print(x  + y)
+
+print(int(x) + int(y))
