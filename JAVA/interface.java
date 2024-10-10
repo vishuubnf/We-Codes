91ed4d2c47ed2a668b1533998e8ecea1e3e2f384
Vishva Pratap Singh Sengar <139144995+vishuubnf@users.noreply.github.com>
2024-10-10 22:04:17 +0530

    Create interface.java

---
+++ JAVA/interface.java
@@ -0,0 +1,23 @@
+// Multiple Inheritance in java
+
+// interface AnimalEat {
+// void eat();
+// }
+// interface AnimalTravel {
+// void travel();
+// }
+// class Animal implements AnimalEat, AnimalTravel {
+// public void eat() {
+// System.out.println("Animal is eating");
+// }
+// public void travel() {
+// System.out.println("Animal is travelling");
+// }
+// }
+// public class lab {
+// public static void main(String args[]) {
+// Animal a = new Animal();
+// a.eat();
+// a.travel();
+// }
+// }
