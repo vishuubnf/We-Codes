c642a6f55d9d4bf0668b66a55d46322e33e103b8
Vishva Pratap Singh Sengar <139144995+vishuubnf@users.noreply.github.com>
2024-10-11 22:28:42 +0530

    Create Child.java

---
+++ JAVA/Child.java
@@ -0,0 +1,25 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class Child implements Mother, Father {
+   public Child() {
+   }
+
+   public void show() {
+      System.out.println("Mother and Father are parents ");
+   }
+
+   public void displayParents() {
+      System.out.println("Two Parents");
+   }
+
+   public void displayChild() {
+      System.out.println("Mother and Father have one child");
+   }
+
+   public static void main(String[] var0) {
+      Child var1 = new Child();
+      System.out.println("Implementation of Hybrid Inheritance in Java");
+      var1.show();
+      var1.displayParents();
+      var1.displayChild();
+   }
+}
